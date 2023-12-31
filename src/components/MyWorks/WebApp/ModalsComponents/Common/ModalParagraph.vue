ba3c5204a243b76d761c190a7d142bb56e889222
b.ponomarenko <bohdan.ponomarenko@wwwest.solutions>
2023-12-31 16:20:17 +0200

    - Installed tailwind

---
+++ src/components/MyWorks/WebApp/ModalsComponents/Common/ModalParagraph.vue
@@ -0,0 +1,5 @@
+<template>
+    <p class="paragraph-dark-color">
+        <slot />
+    </p>
+</template>
--- src/components/MyWorks/WebApp/ModalsComponents/LogMyHours/LMHMain.vue
+++ src/components/MyWorks/WebApp/ModalsComponents/LogMyHours/LMHMain.vue
@@ -1,14 +1,15 @@
 <template>
     <ModalLayout :modalCaption="getNestedValue(modalData, 'title', null)">
-        LMH
-        text
-        text
+        <ModalParagraph>
+            In a two-person team, I led as a full-stack developer, overseeing project initiation, driving feature innovation, and ensuring quality through detailed bug fixing and staging management.
+        </ModalParagraph>
     </ModalLayout>
 </template>
 
 <script setup lang="ts">
 import ModalLayout from "@/components/MyWorks/WebApp/ModalsComponents/Common/ModalLayout.vue";
 import { getNestedValue } from "@/helpers/helpers";
+import ModalParagraph from "@/components/MyWorks/WebApp/ModalsComponents/Common/ModalParagraph.vue";
 
 defineProps({
     modalData: {
